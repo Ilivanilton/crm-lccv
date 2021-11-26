c742c7e636966327235805abac214448a96d160c
ilivanilton <ilivanilton@gmail.com>
2021-11-26 14:48:45 -0300

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FuncionariosComponent } from './modules/funcionarios/funcionarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FuncionariosComponent
   ],
   imports: [
     BrowserModule,
